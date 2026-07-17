cbd72b986bf89aa8b8c7ec388dff9ae728f13989
M-Munoz-cl <290170622+M-Munoz-cl@users.noreply.github.com>
2026-07-17 18:46:49 -0400

    secrets agregado a config.py

--- src/config.py
+++ src/config.py
@@ -1,9 +1,13 @@
 from dotenv import load_dotenv
 import os
+import streamlit as st
 
 load_dotenv()
 
 TICKET = os.getenv("TICKET")
+
+if not TICKET:
+    TICKET = st.secrets("TICKET")
 
 BASE_URL = "https://api2.mercadopublico.cl"
 
